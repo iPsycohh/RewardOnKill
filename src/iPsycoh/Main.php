4b471782db139ad2e7a66d2c2dec2509f99ce184
iPsycohh <42064439+iPsycohh@users.noreply.github.com>
2018-08-10 01:28:52 +0200

    api 3.1.x support

---
+++ src/iPsycoh/Main.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace iPsycoh;
+
+//Plugin
+use pocketmine\plugin\PluginBase;
+//Listener
+use pocketmine\event\Listener;
+//akaso
+use pocketmine\Server;
+use pocketmine\item\Item;
+//Player
+use pocketmine\Player;
+//EntityNEvent
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable(){
+
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+$this->getLogger()->info("§cPlugin Activated");
+}
+
+public function onDeath(PlayerDeathEvent  $event){
+
+$player = $event->getPlayer();
+
+if($player instanceof Player){
+$cause = $player->getLastDamageCause(); 
+if($cause instanceof EntityDamageByEntityEvent){ 
+$strunz = $cause->getDamager();
+
+$item1 = Item::get(Item::GOLDEN_APPLE);			
+$strunz->getInventory()->setItem(3, $item1);		
+                          }
+                }
+        }
+}
